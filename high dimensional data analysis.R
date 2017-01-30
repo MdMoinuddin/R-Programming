f7bd34c26540ea65d04285267712554fbebaddfd
MdMoinuddin <moin.icddrb@gmail.com>
2017-01-31 00:11:59 +0100

    Add files via upload

---
+++ high dimensional data analysis.R
@@ -0,0 +1,9 @@
+#harvard X, High-Dimensional Data Analysis
+
+install.packages("devtools")
+library(devtools)
+install_github("genomicsclass/tissuesGeneExpression")
+
+install.packages("tissuesGeneExpression")
+library(tissusesGeneExpression)
+data()
